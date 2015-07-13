4b9bec819e6158fad580c7bb3906036746c9d258
Christopher Collopy <chelco91@gmail.com>
2015-07-13 16:00:01 -0600

    Updated index.php added .css file and image

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>About Me!!</title>
+		<link rel="stylesheet" href="css/styles.css">
+	</head>
+	<body>
+		<img src="images/image1.gif">
+
+		<p></p>
+		<p></p>
+	</body>
+</html>
